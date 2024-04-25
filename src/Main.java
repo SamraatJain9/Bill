2cf0a9e458fa6fe51162139cc214d940665c5758
Samraat-LK789 <samraat1official8@gmail.com>
2024-04-25 16:50:42 +0530

    Bill Format Setting

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("               ~RESTAURANT NAME~");
+        System.out.println("===============================================");
+        System.out.println("               RESTAURANT");
+        System.out.println("            141.PEARL.STREET");
+        System.out.println("      Compliments of GENES AAA restaurant");
+        System.out.println("===============================================");
+
+    }
+}
